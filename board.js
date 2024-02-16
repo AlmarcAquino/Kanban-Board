14cf6da7c5236638711a368102f8ee6022ef811c
AlmarcAquino <almarcfaquino@gmail.com>
2024-02-15 22:23:34 -0800

    Added form handling

--- board.js
+++ board.js
@@ -1,6 +1,3 @@
-// TODO:
-//    open modal based on modal btn clicked
-//    Add ability to create new tasks
 //
 // GLOBALS
 //
@@ -8,14 +5,15 @@
 //
 // MODALS
 //
-// Get list of all button which open a modal
-const openModalBtns = [...document.querySelectorAll('[data-modal-open]')];
-const modalContainers = [
-  ...document.querySelectorAll('[data-modal-container]'),
-];
+// Get list of all buttons which open a modal
+const openModalBtns = document.querySelectorAll('[data-modal-open]');
+// Get list of all modal containers
+const modalContainers = document.querySelectorAll('[data-modal-container]');
+// Get list of all task columns
+const taskColumnContainers = [...document.querySelectorAll('.task-column')];
 
-// Functions to open/close modal
-const openModal = (modalName) => {
+// Functions to open/close modal and handle form submissions
+const manageModal = (modalName, targetColumn) => {
   let modal = null;
   modalContainers.forEach((container) => {
     if (container.dataset.modalContainer === modalName) modal = container;
@@ -28,6 +26,19 @@ const openModal = (modalName) => {
       closeModal(modal);
     }
   });
+
+  // When the form is submitted:
+  // Clear the form's inputs, close the modal
+  // If a task modal was submitted, add the task to the column
+  const modalForm = modal.querySelector('form');
+  const formName = modalForm.dataset.formName;
+  const inputs = modalForm.querySelectorAll('input');
+  modalForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (formName === 'task') addTaskToColumn(inputs, targetColumn);
+    clearFormInputs(inputs);
+    closeModal(modal);
+  });
 };
 
 const closeModal = (modal) => {
@@ -38,12 +49,25 @@ const closeModal = (modal) => {
 openModalBtns.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     const clickedBtn = e.target;
-    determineModal(clickedBtn);
+    const targetColumn = clickedBtn.closest('.task-column');
+    determineModal(clickedBtn, targetColumn);
   });
 });
 
 // Determine which modal to open based on data attribute of button
-const determineModal = (btn) => {
+const determineModal = (btn, targetColumn) => {
   const modalName = btn.dataset.modalOpen;
-  openModal(modalName);
+  manageModal(modalName, targetColumn);
+};
+
+const clearFormInputs = (inputs) => {
+  inputs.forEach((input) => {
+    input.value = '';
+  });
+};
+
+// TODO:
+// Add new task with user inputs
+const addTaskToColumn = (inputs, column) => {
+  console.log('NOW ADD THE TASK');
 };
