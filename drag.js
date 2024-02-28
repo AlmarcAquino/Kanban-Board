674ee5110180365c773fd4234a1031dff1892dc9
AlmarcAquino <almarcfaquino@gmail.com>
2024-02-27 21:23:18 -0800

    Renamed 'updateTasks' function to 'updateTaskList' for consistency

--- drag.js
+++ drag.js
@@ -1,4 +1,4 @@
-import { updateTasks } from './board.js';
+import { updateTaskList } from './board.js';
 
 // Get all tasks and task columns
 const taskTiles = document.querySelectorAll('.task');
@@ -14,7 +14,7 @@ taskColumns.forEach((column) => {
   // Remove 'is-dragging' class when task is no longer being dragged
   column.addEventListener('dragend', (e) => {
     if (e.target.matches('.task')) e.target.classList.remove('is-dragging');
-    updateTasks();
+    updateTaskList();
   });
 
   column.addEventListener('dragover', (e) => {
